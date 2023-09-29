1e6ebbc697c2a8d5b55bf0cd42f820f05bd401d6
LuizGusttavo <luiz.gusttavo@lavid.ufpb.br>
2023-09-29 09:32:07 -0300

    create models and attaching to db

--- estudos/polls/models.py
+++ estudos/polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+# Aqui, cada modelo é representado por uma classe derivada da classe django.db.models.Model. 
+# Cada modelo possui alguns atributos de classe, as quais por sua vez representa um campo do banco de dados no modelo.
+
+# Cada campo é representado por uma instância de uma classe Field
+# por exemplo, CharField para campos do tipo caractere e DateTimeField para data/hora. 
+# Isto diz ao Django qual tipo de dado cada campo contém
