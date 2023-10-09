7a2f16b71ac74e3f47f6cf1ea50c43cd9378b511
LuizGusttavo <luiz.gusttavo@lavid.ufpb.br>
2023-10-09 17:34:48 -0300

    Mudança nas views index, detail e results

--- estudos/polls/views.py
+++ estudos/polls/views.py
@@ -1,29 +1,30 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 ""
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # Invoca o documento html criado em templates
-    template = loader.get_template('polls/index.html')
-    # Através do dicionário de contexto o html recebe as variáveis nele usadas
-    context = {
-        'latest_question_list': latest_question_list    
-    }
-    # Podemos gerar a resposta com HttpResponse(), usando a lib django.http
-    # Ou pode-se usar a função render() passando os 3 parâmetros como a seguir:
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id) 
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self) -> QuerySet[Any]:
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
